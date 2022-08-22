b5b06a73905481d43456710a51b825a55b6d7876
devephy <litephantus@gmail.com>
2022-08-22 13:02:22 +0300

    Configured routes directory

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+
+//  Created variables for the environments needed to run this the node app
+
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+// This app Directs Root path "/"===="/api/goals" to this file folder location
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(port, () => {
+    console.log(`Server Started on ${port} `);
+});
