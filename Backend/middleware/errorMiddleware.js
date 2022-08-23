110036440e73647f433138e74f9fd4e58a6bdfd1
devephy <litephantus@gmail.com>
2022-08-24 00:03:36 +0300

    configuration final set up

--- Backend/middleware/errorMiddleware.js
+++ Backend/middleware/errorMiddleware.js
@@ -5,11 +5,9 @@ const errorHandler = (err, req, res, next) =>{
     
     res.json({
         message : err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+        stack : process.env.NODE_ENV === 'production' ? null : err.stack
     })
 }
-
-
 
 
 
