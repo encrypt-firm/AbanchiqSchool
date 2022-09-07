c1bba723313a2588d340c973b98987e8173da665
devephy <litephantus@gmail.com>
2022-09-07 03:52:34 +0300

    user configurations

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,11 +1,35 @@
+//@DESC  REGISTER NEW USER ****POST METHOD*****
+// @ROUTE   /api/user
+// @Access   Public
 const registerUser =(req, res) =>{
     res.status(200).json({
         message : 'User Registered'
     })
-
+}
+//@DESC  Authenticate  USER ****POST METHOD*****
+// @ROUTE   /api/user/login
+// @Access   Public
+const loginUser =(req, res) =>{
+    res.status(200).json({
+        message : 'logged in user'
+    })
+}
+//@DESC  get USER profile/data ****GET METHOD*****
+// @ROUTE   /api/user/profile
+// @Access   Public
+const userProfile =(req, res) =>{
+    res.status(200).json({
+        message : 'User Profile'
+    })
 }
 
 
+
+
+
+
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser,
+    userProfile
 }
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,11 +2,15 @@ const express = require('express')
 const router = express.Router()
 const {
     registerUser,
+    loginUser,
+    userProfile
 } = require('./../controllers/userController')
 
 
 router.post('/', registerUser);
+router.post('/login', loginUser);
+router.get('/profile', userProfile);
 
 
 
-module.exports = router
+module.exports = router 
