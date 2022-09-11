3223379b10bd7800b9bd4d50e7b75349683f311c
devephy <litephantus@gmail.com>
2022-09-11 23:57:15 +0300

    user controller configurations

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -13,13 +13,44 @@ const registerUser =asyncHandler( async(req, res) =>{
     if (!name || !email || !password){
         res.status(400)
         throw new Error('Please add all fiels')
-    } else {
-        
+    } 
+    
+    const userExist = await User.findOne({email})
+
+    if (userExist){
+        res.status(400)
+        throw new Error('oops! The user Exist, use nother email')
     }
-res.status(200).json({
-    message : 'User Registered'
+
+    // Hashing passwords
+
+    const salt = await bcrypt.genSalt(10)
+
+    const hashedpassword = await bcrypt.hash(password, salt)
+
+
+
+    // Create User
+    const user = await User.create({
+        name,
+        email,
+        password: hashedpassword
+
+    })
+    if(user){
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid User data')
+    }
+
 })
-})
+
+
 //@DESC  Authenticate  USER ****POST METHOD*****
 // @ROUTE   /api/user/login
 // @Access   Public
