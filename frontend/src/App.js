8996645f3156ec4901bf0e42e92cc00a62b4162e
devephy <litephantus@gmail.com>
2023-04-03 17:28:22 +0300

    pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Register from './pages/register/Register'
+import Login from './pages/login/Login'
+import Dashboard from './pages/dashboard/Dashboard'
+// import Logout from './components/logout/Logout'
+
 
 function App() {
   return (
-    <div className="App">
-        <h1>Homepage</h1>
-    </div>
+
+    <>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path='/' element={<Dashboard/>} />
+            <Route path='/register' element={<Register/>} />
+            <Route path='/login' element={<Login/>} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
