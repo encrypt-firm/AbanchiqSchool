68b1c81b3da56f83e34650e8f6b84df80d9d8f7e
devephy <litephantus@gmail.com>
2023-05-11 10:04:26 +0300

    login and register

--- frontend/src/pages/register/Register.js
+++ frontend/src/pages/register/Register.js
@@ -1,25 +1,12 @@
-import React from 'react'
-import './register.css'
-import { BsFillLockFill } from 'react-icons/bs'
-import { FcGoogle } from 'react-icons/fc'
-import { GoHome } from 'react-icons/go'
-import { TfiHelpAlt } from 'react-icons/tfi'
-import { FaUserAlt } from 'react-icons/fa'
-import { HiOutlineMail } from 'react-icons/hi'
-import { MdOutlineWifiPassword } from 'react-icons/md'
-import { AiOutlineWindows } from 'react-icons/ai'
 import { useState, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
-import { register, reset } from './../../features/auth/authSlice'
-import Spinner from './../../components/Spinner/Spinner'
+import { FaUser } from 'react-icons/fa'
+import { register, reset } from '../../features/auth/authSlice'
+import Spinner from '../../components/Spinner/Spinner'
 
 function Register() {
-    const icon = {
-        color: "black",
-        fontSize: "35px"
-    }
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -28,6 +15,7 @@ function Register() {
     })
 
     const { name, email, password, password2 } = formData
+
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
@@ -39,6 +27,7 @@ function Register() {
         if (isError) {
             toast.error(message)
         }
+
         if (isSuccess || user) {
             navigate('/')
         }
@@ -47,16 +36,15 @@ function Register() {
     }, [user, isError, isSuccess, message, navigate, dispatch])
 
     const onChange = (e) => {
-
         setFormData((prevState) => ({
-            prevState,
-            [e.target.name]: e.target.value
+            ...prevState,
+            [e.target.name]: e.target.value,
         }))
-
     }
 
     const onSubmit = (e) => {
         e.preventDefault()
+
         if (password !== password2) {
             toast.error('Passwords do not match')
         } else {
@@ -73,54 +61,72 @@ function Register() {
     if (isLoading) {
         return <Spinner />
     }
+
     return (
-        <div className='registerSection'>
-            <div className="cover-message">
-                <div className="login-message">
-                    <div className="message-wrapper">
-                        <div className="message">Handbags</div>
-                        <div className="message">Shoes</div>
-                        <div className="message">Watches</div>
-                        <div className="message">Oils</div>
-                        <div className="message">Perfumes</div>
+        <>
+            <section className='heading'>
+                <h1>
+                    <FaUser /> Register
+                </h1>
+                <p>Please create an account</p>
+            </section>
+
+            <section className='form'>
+                <form onSubmit={onSubmit}>
+                    <div className='form-group'>
+                        <input
+                            type='text'
+                            className='form-control'
+                            id='name'
+                            name='name'
+                            value={name}
+                            placeholder='Enter your name'
+                            onChange={onChange}
+                        />
                     </div>
-                </div>
-            </div>
-            <form onSubmit={onSubmit}>
-
-                <div class="segment">
-                    <h1>Sign up</h1>
-                    < AiOutlineWindows style={icon} />
-                </div>
-
-                <label>
-                    <input type="text" placeholder="USERNAME" id='name' value={name} name='name' onChange={onChange} />
-                    < FaUserAlt />
-                </label>
-                <label>
-                    <input type="email" placeholder="Enter Email Address" id='email' value={email} name='email' onChange={onChange} />
-                    < HiOutlineMail />
-                </label>
-                <label>
-                    <input type="password" placeholder="Enter Password" id='password' value={password} name='password' onChange={onChange} />
-                    < MdOutlineWifiPassword />
-                </label>
-                <label>
-                    <input type="password" placeholder="Confirm your Password" id='password2' value={password2} name='password2' onChange={onChange} />
-                    < MdOutlineWifiPassword />
-                </label>
-                <button class="red" type="submit"><BsFillLockFill /> Sign up</button>
-
-                <div class="segment">
-                    <Link to='/'><button class="unit" type="button">< GoHome /></button></Link>
-                    <button class="unit" type="button">< FcGoogle /></button>
-                    <button class="unit" type="button">< TfiHelpAlt /></button>
-                </div>
-
-
-            </form>
-        </div>
+                    <div className='form-group'>
+                        <input
+                            type='email'
+                            className='form-control'
+                            id='email'
+                            name='email'
+                            value={email}
+                            placeholder='Enter your email'
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            type='password'
+                            className='form-control'
+                            id='password'
+                            name='password'
+                            value={password}
+                            placeholder='Enter password'
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            type='password'
+                            className='form-control'
+                            id='password2'
+                            name='password2'
+                            value={password2}
+                            placeholder='Confirm password'
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <button type='submit' className='btn btn-block'>
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </section>
+        </>
     )
 }
+
 
 export default Register
